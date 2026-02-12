7ab26ea7faa13e71234a8b020eeff85c79fdb46d
Zey Gmr <zeynabgomar.1385@gmail.com>
2026-02-12 15:27:42 +0330

    Create User.java

---
+++ src/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+public class User {
+    private String username;
+    private String password;
+    private String name;
+    public User() {}
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getName() { return name; }
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public void setName(String name) { this.name = name; }
+}
